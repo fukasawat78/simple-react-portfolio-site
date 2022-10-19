f461b723a244b68726b796fefda8c2c154b71cf5
fukasawat78 <fukasawat78@bbwcfs.local127.0.0.1>
2022-10-19 16:02:35 +0900

    modified

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,12 @@
 import MotionHoc from "./MotionHoc";
+import background from "../assets/home.jpg";
 
 const HomeComponent = () => {
-    return <h1>Home</h1>;
+    return (
+        <div style={{ backgroundImage: `url(${background})` }}>
+            <h1>Home</h1>;
+        </div>
+    )
 };
 
 const Home = MotionHoc(HomeComponent);
